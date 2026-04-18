ebd3eb50c88df1125c00a3ccd69a0f72330f09d5
oliik <131269027+oliik2013@users.noreply.github.com>
2026-04-18 11:22:09 +0200

    Fix workflows and add new and change extra

--- extras/misty-loop.py
+++ extras/misty-loop.py
@@ -1,6 +1,28 @@
 import os
+import argparse
 from time import sleep
 
+parser = argparse.ArgumentParser(description="Misty loop")
+
+parser.add_argument("--istesting", type=bool, help="Used for testing, by limiting the loop to 25 secounds (3 images).")
+parser.add_argument("--size", "-s", type=int, help="Size of Misty images.")
+
+args = parser.parse_args()
+
+if args.size != None:
+    commandcli = "misty-cli -s " + str(args.size)
+else:
+    commandcli = "misty-cli"
+
+if args.istesting == True:
+    os.system(commandcli)
+    sleep(10)
+    os.system(commandcli)
+    sleep(10)
+    os.system(commandcli)
+    sleep(5)
+    exit()
+
 while True:
-    os.system("misty-cli")
+    os.system(commandcli)
     sleep(10)
